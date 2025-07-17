54d5048584175601dbae60182a796d30a87f5d98
Srikanth Kakumanu <srikanthkakumanu@yahoo.com>
2025-07-18 02:09:32 +0530

    Fixed Vault Integration errors in api-gateway, user-service,books-service

--- src/main/java/user/util/JWTGenerator.java
+++ src/main/java/user/util/JWTGenerator.java
@@ -16,19 +16,26 @@ import javax.crypto.SecretKey;
 @Slf4j
 public class JWTGenerator {
 
-    @Value("${jwt-expiry-ttl}")
-    private long jwtExpiryTtl;
-    private static final String secret = "Secret123@@321terceSSecret123@@321terceS@321terceS";
+    private final long jwtExpiryTtl;
 
-    // Option 1: Option 1: Using Keys.hmacShaKeyFor (recommended for fixed-length secrets)
+    private final SecretKey key;
+
+    /*
+    Note:-
+    Option 1: Using Keys.hmacShaKeyFor (recommended for fixed-length secrets)
     SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-    // Option 2: Using Keys.secretKeyFor (generates a random secret key, good for dynamic secrets)
-    //private static final SecretKey key = Jwts.SIG.HS512.key().build();
+    Option 2: Using Keys.secretKeyFor (generates a random secret key, good for dynamic secrets)
+    private static final SecretKey key = Jwts.SIG.HS512.key().build();
+    */
+    public JWTGenerator(@Value("${jwt-expiry-ttl}") long jwtExpiryTtl,
+                        @Value("${jwt-secret}") String secret) {
+        this.jwtExpiryTtl = jwtExpiryTtl;
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(Authentication authentication) {
         String username = authentication.getName();
         long nowMillis = System.currentTimeMillis();
-        System.out.println("jwtExpiryTtl : " + jwtExpiryTtl);
         long ttlMillis = nowMillis + jwtExpiryTtl;
         log.info("JWT Info - [nowMillis: {}, jwtExpiryTtl: {} ttlMillis: {}]", nowMillis, jwtExpiryTtl, ttlMillis);
 
